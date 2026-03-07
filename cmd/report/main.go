2d4585788faeb0cf8fdc10f9d38bf758ab2b312c
Jakob Borg <jakob@kastelo.net>
2026-03-07 13:05:44 +0100

    fix: cmd/report error handling

--- cmd/report/main.go
+++ cmd/report/main.go
@@ -12,21 +12,26 @@ func main() {
 	doc, err := sie.Parse(os.Stdin)
 	if err != nil {
 		slog.Error("Error parsing SIE file", "error", err)
+		os.Exit(1)
 	}
 
 	bs, err := excel.ResultXLSX(doc)
 	if err != nil {
 		slog.Error("Error creating Excel file", "error", err)
+		os.Exit(1)
 	}
 	if err := os.WriteFile("result.xlsx", bs, 0o644); err != nil {
 		slog.Error("Error writing Excel file", "error", err)
+		os.Exit(1)
 	}
 
 	bs, err = excel.BalanceXLSX(doc)
 	if err != nil {
 		slog.Error("Error creating Excel file", "error", err)
+		os.Exit(1)
 	}
 	if err := os.WriteFile("balances.xlsx", bs, 0o644); err != nil {
 		slog.Error("Error writing Excel file", "error", err)
+		os.Exit(1)
 	}
 }
